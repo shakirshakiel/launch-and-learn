8e9a23892aa52aee02f1d599714bc27c8a82cb6e
Shakir Shakiel <shakirshakiel@gmail.com>
2020-07-08 18:51:57 +0530

    Enable JMX monitoring

--- zookeeper/roles/zookeeper/templates/zookeeper-env.sh
+++ zookeeper/roles/zookeeper/templates/zookeeper-env.sh
@@ -1,2 +1,4 @@
 export ZK_SERVER_HEAP=2048
-export SERVER_JVMFLAGS="-Xms512m"
+export SERVER_JVMFLAGS="-Xms512m -Djava.rmi.server.hostname={{ hostvars[inventory_hostname]['ansible_host'] }} -Dcom.sun.management.jmxremote.rmi.port=8090 -Dcom.sun.management.jmxremote.local.only=false"
+export JMXPORT=8090
+#export JMXLOG4J=false
